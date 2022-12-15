412d78f9d7b9a714c3d186c8e3271d61a2e979bd
Daniel <gorn225@interia.pl>
2022-12-15 09:14:23 +0100

    remove old config

--- frontend/src/config/my-api.config.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-export default axios.create({
-  baseURL: 'http://localhost:8090/api/',
-  timeout: 5000,
-  headers: {
-    'X-Auth-Token': 'f2b6637ddf355a476918940289c0be016a4fe99e3b69c83d',
-    'Content-Type': 'application/json'
-  }
-})
